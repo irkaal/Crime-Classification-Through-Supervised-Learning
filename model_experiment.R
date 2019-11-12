af9b22df533193c854707af9fbd62980286adac4
irkaal <irkaalv@gmail.com>
2019-11-12 00:47:47 -0800

    Refactor workflow in model_experiment.R to use random hyperparameter search and adaptive sampling

--- model_experiment.R
+++ model_experiment.R
@@ -1,85 +1,185 @@
-
-# Setup
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 source('misc.R')
 source('pre_process.R')
-loadPackages(c(
-  'data.table', 'slam', 'Matrix', 'mltools',  # For performance boost
-  'caret', 'xgboost', 'h2o',                  # For parameter tuning, H2O (http://h2o-release.s3.amazonaws.com/h2o/rel-yau/10/index.html)
-  'foreach', 'parallel', 'doParallel'         # For parallelization
-))
-registerDoParallel(detectCores() / 2)
+loadPackages(c('data.table', 'slam', 'caret', 'xgboost', 'plyr', 'h2o', 'xlsx', 'parallel', 'doParallel'), quietly = T)
+registerDoParallel(2)
 
 
-#
-# Data Pre-processing
-#
 
-# Load Data
-path <- unzip('data/sf-crime.zip', 'train.csv')
-crime_data <- fread(path); invisible(file.remove(path))
-# path <- unzip('data/sf-crime.zip', 'test.csv')
-# crime_data_test <- fread(path); invisible(file.remove(path))
+# 1. Data Pre-processing and feature engineering
 
-# Clean Data
-crime_data <- preProcess(crime_data)
-# crime_data_test <- preProcess(crime_data_test)
+# Load train data
+# TODO: REMOVE
+train <- fread('data/clean.csv')
+# path <- unzip('data/sf-crime.zip', 'train.csv')
+# train <- fread(path); invisible(file.remove(path))
 
-#
-# Model Tuning
-#
+# Clean data
+# train <- preProcess(train)
 
+# Prepare data for training
+y_train <- factor(make.names(as.numeric(factor(train$Category))))
+x_train <- train[, -68]
+rm(train)
+
+# Correlation Plot
+# corr_mat <- cor(x_train)
+# corrplot::corrplot(corr_mat)
+
+# Remove highly correlated features
+# highCorr <- findCorrelation(corr_mat, cutoff = 0.75)
+# predictors(highCorr)
+# x_train <- x_train[, -highCorr]
+
+# Downsampling test
+# ds <- downSample(x = x_train, y = y_train)
+# x_train <- ds[, -68]
+# y_train <- factor(ds$Class)
+# rm(ds)
+
+
+
+# Parameter tuning and feature selection
+
+# Random Control - Random Hyperparameter Search
+randCtrl <- trainControl(method = 'repeatedcv', 
+                         number = 10,
+                         repeats = 10,
+                         classProbs = T,
+                         summaryFunction = mnLogLoss,
+                         search = 'random') 
+
+# Adaptive Resampling - Racing-type algorithm
+# Generalized Least Squares Model
+adaptGlsCtrl <- trainControl(method = 'adaptive_cv',
+                             number = 10, 
+                             repeats = 10,
+                             adaptive = list(min = 5, alpha = 0.05, 
+                                             method = 'gls', complete = T),
+                             classProbs = T,
+                             summaryFunction = mnLogLoss,
+                             search = 'random')
+# Bradley-Terry model
+adaptBTCtrl <- trainControl(method = 'adaptive_cv',
+                            number = 10, 
+                            repeats = 10,
+                            adaptive = list(min = 5, alpha = 0.05, 
+                                            method = 'BT', complete = T),
+                            classProbs = T,
+                            summaryFunction = mnLogLoss,
+                            search = 'random')
+
+
+
+# XGBoost - eXtreme Gradient Boosting
+
+# Tree
 set.seed(2019)
+xgbTreeModel <- train(x = x_train, 
+                      y = y_train,
+                      method = 'xgbTree', 
+                      metric = "logLoss", 
+                      trControl = adaptBTCtrl, 
+                      tuneLength = 15)
+write.xlsx(data.frame(xgbTreeModel$bestTune), file = "data/param_tuning/xgbTreeModel.xlsx", sheetName = "sheet1", row.names = F)
+if (nrow(xgbTreeModel$results)) write.xlsx(data.frame(xgbTreeModel$results), file = "data/param_tuning/xgbTreeModel.xlsx", sheetName = "sheet2", append = T, row.names = F)
 
-# Split data.table into a feature sparse matrix and a response vector
-train_data <- mltools::sparsify(crime_data[, -1])
-train_label_num <- as.numeric(factor(crime_data$Category)) - 1
-train_label <- factor(make.names(train_label_num))
 
-# XGBoost (Tree)
+# Linear Model
+set.seed(2019)
+xgbLinearModel <- train(x = x_train, 
+                        y = y_train,
+                        method = 'xgbLinear', 
+                        metric = "logLoss", 
+                        trControl = adaptBTCtrl, 
+                        tuneLength = 15)
+write.xlsx(data.frame(xgbLinearModel$bestTune), file = "data/param_tuning/xgbLinearModel.xlsx", sheetName = "sheet1", row.names = F)
+if (nrow(xgbLinearModel$results)) write.xlsx(data.frame(xgbLinearModel$results), file = "data/param_tuning/xgbLinearModel.xlsx", sheetName = "sheet2", append = T, row.names = F)
 
-# Direct (No Tuning, only nrounds)
-dtrain <- xgb.DMatrix(data = train_data, label = train_label_num)
-bst <- xgboost(data = dtrain, booster = 'gbtree', objective = 'multi:softmax', num_class = 39, 
-               eval_metric = 'mlogloss', nrounds = 20)
-pred <- predict(bst, train_data)
 
-# Caret
-xgbControl <- trainControl(method = 'repeatedcv', number = 10, repeats = 10,
-                           allowParallel = T,
-                           verboseIter = T, classProbs = T,
-                           savePredictions = T,  returnData = F,
-                           summaryFunction = mnLogLoss)
-xgbGrid <- expand.grid(max_depth = c(6),
-                       nrounds = c(50, 100),
-                       eta = c(0.2, 0.3, 0.4),
-                       gamma = c(0, 0.25, 0.5, 0.75, 1), # Regularization
-                       colsample_bytree = 1,
-                       min_child_weight = 1,
-                       subsample = c(0.5, 1))
-xgbModel <- train(x = train_data, y = train_label,
-                  method = 'xgbTree', metric = "logLoss", 
-                  trControl = xgbControl, tuneGrid = xgbGrid)
+# Dropouts meet Multiple Additive Regression Trees
+set.seed(2019)
+xgbDARTModel <- train(x = x_train, 
+                      y = y_train,
+                      method = 'xgbDART', 
+                      metric = "logLoss", 
+                      trControl = adaptBTCtrl, 
+                      tuneLength = 15)
+write.xlsx(data.frame(xgbDARTModel$bestTune), file = "data/param_tuning/xgbDARTModel.xlsx", sheetName = "sheet1", row.names = F)
+if (nrow(xgbDARTModel$results)) write.xlsx(data.frame(xgbDARTModel$results), file = "data/param_tuning/xgbDARTModel.xlsx", sheetName = "sheet2", append = T, row.names = F)
 
-pred <- predict(xgbModel, train_data)
 
-# H2O (GBM)
+# H2O Gradient Boosting Machines 
 
-h2o.init(); options("h2o.use.data.table" = T)
-h2o_train <- as.h2o(crime_data[, -1])
-gbmControl <- trainControl(method = 'none')
-gbmControl <- trainControl(method = 'repeatedcv', number = 10, repeats = 10,
-                           allowParallel = T,
-                           verboseIter = T, classProbs = T,
-                           savePredictions = T,  returnData = F,
-                           summaryFunction = mnLogLoss)
-gbmGrid <- expand.grid(ntrees = 50, 
-                       max_depth = c(5), 
-                       min_rows = c(10), 
-                       learn_rate = c(0.1), 
-                       col_sample_rate = c(1))
-gbmModel <- train(x = h2o_train, y = train_label,
-                  method = 'gbm_h2o', metric = "logLoss", 
-                  trControl = gbmControl, tuneGrid = gbmGrid)
+# Setup
+# h2o.init()
+# options('h2o.use.data.table' = T)
+# x_train_h2o <- as.h2o(x_train)
+# y_train_h2o <- factor(make.names(y_train))
+# rm(list = c('x_train', 'y_train'))
 
-pred <- predict(gbmModel, train_data)
+# Default GBM
+# set.seed(2019)
+# gbmModel <- train(x = x_train_h2o, 
+#                   y = y_train_h2o,
+#                   method = 'gbm_h2o', 
+#                   metric = "logLoss", 
+#                   trControl = adaptBTCtrl, 
+#                   tuneLength = 15)
+
+# gbmPred <- predict(gbmModel, x_train_h2o)
+
+# Save results
+# fwrite(data.table(gbmPred), 'data/prediction/gbmModelPred.csv')
+# write.xlsx(data.frame(gbmModel$results),
+#            file = "data/param_tuning/gbmModel.xlsx", sheetName = "sheet1", row.names = F)
+# write.xlsx(data.frame(gbmModel$bestTune),
+#            file = "data/param_tuning/gbmModel.xlsx", sheetName = "sheet2", append = T, row.names = F)
+
+
+# TODO: 
+# svmLinear3
+# lssvmLinear 
+# lssvmPoly
+# svmBoundrangeString
+# svmExpoString
+# svmLinear
+# svmPoly
+# svmRadial
+# svmRadialCost
+# svmRadialSigma
+# svmSpectrumString
+# RSofia SGD - Not caret
+
+
+# TODO: 
+# ranger
+# Random Forest by Randomization
+# Random Ferns
+
+
+# TODO: 
+# Sparse LDA
+# Penalized LDA
+# Bagged FDA
+
+
+# TODO: 
+# Nearest Shrunken Centroids
+
+
+
+# 3. Prediction
+
+# Load test data
+# path <- unzip('data/sf-crime.zip', 'test.csv')
+# test <- fread(path); invisible(file.remove(path))
+# rm(path)
+
+# Clean data
+# test <- preProcess(test)
+
+# Predict
+
+# Clean up
+# rm(list = ls())
