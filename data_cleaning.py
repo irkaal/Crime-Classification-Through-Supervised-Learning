f535302aaa4b45f7b26f7de91c19084a13edb73f
irkaal <irkaalv@gmail.com>
2019-11-15 20:28:56 -0800

    Add additional columns

--- data_cleaning.py
+++ data_cleaning.py
@@ -6,6 +6,7 @@ import pandas as pd
 from pandas.tseries.holiday import USFederalHolidayCalendar
 from sklearn.preprocessing import StandardScaler
 from zipfile import ZipFile
+from sklearn.decomposition import PCA
 # Global variables
 i = 0
 max_i = 0
@@ -53,14 +54,21 @@ def main_clean(dataset, center_scale = False):
         update_progress('Encoding Hour')
         dataset = encode_cyclic(dataset, 'Hour', date_indices.hour + date_indices.minute / 60, 24)
         
-        # TODO: Encode Event (Label encoding - 0 for not an event day and 1 for an event day)
-        update_progress('Encoding Event')
-        dataset = encode_event(dataset, date_indices.date)
+        # Encode time into 6-Hour Period
+        dataset = encode_period(dataset, date_indices.hour)
         
-        # TODO: Encode Geospatial
-        update_progress('Encoding Geospatial') 
-        dataset = encode_geospatial(dataset)
+        # Encode haversine distance to nearest police station 
+        update_progress('Encoding NearestStation_Hav') 
+        dataset = encode_nearest_hav(dataset)
         
+        # TODO: Add comments for these additional features
+        dataset = encode_nearest_man(dataset)
+        dataset = polar_coord(dataset)
+        dataset = rotate_coord(dataset)
+        dataset = rotate_pca(dataset)
+        dataset = encode_nearest_bearing_hav(dataset)
+        dataset = encode_nearest_bearing_man(dataset)
+
         # Drop unused columns
         update_progress('Dropping unused columns') 
         dataset = dataset.drop(columns = colList)
@@ -72,23 +80,52 @@ def main_clean(dataset, center_scale = False):
     end() # End progress tracker
     return dataset
 
+
 def encode_patrol_div(dataset):
     division = {'BAYVIEW': 0, 'MISSION': 0, 'PARK': 0, 'RICHMOND': 0, 'TARAVAL': 0, 
                 'CENTRAL': 1, 'INGLESIDE': 1, 'NORTHERN': 1, 'SOUTHERN': 1, 'TENDERLOIN': 1} 
     dataset['PatrolDivision'] = dataset['PdDistrict'].map(division) 
     return dataset
 
-def encode_event(dataset, date_indices_date):
-    holidays = pd.Series(USFederalHolidayCalendar().holidays(start = '2003-01-01', end = '2015-05-13'))
-    # This will not include original time info which makes comparison possible
-    datetime = pd.to_datetime(date_indices_date)
-    dataset['Event'] = pd.Series(datetime).isin(holidays).astype(int)
+def encode_period(dataset, hour):
+    period = np.floor(hour / 6) % 6
+    dataset['00:00-05:59'] = (period == 0).astype(int)
+    dataset['06:00-11:59'] = (period == 1).astype(int)
+    dataset['12:00-17:59'] = (period == 2).astype(int)
+    dataset['18:00-23:59'] = (period == 3).astype(int)
     return dataset
 
-def encode_geospatial(dataset):
+def rotate_coord(dataset):
+    # Source - https://en.wikipedia.org/wiki/Rotation_of_axes
+    # x' = x * cosθ + y * sinθ
+    # y' = y * cosθ - x * sinθ
+    x, y = dataset['X'], dataset['Y']
+    dataset['R30_X'] = (x * np.cos(30)) + (y * np.sin(30))
+    dataset['R30_Y'] = (y * np.cos(30)) - (x * np.sin(30))
+    dataset['R45_X'] = (x * np.cos(45)) + (y * np.sin(45))
+    dataset['R45_Y'] = (y * np.cos(45)) - (x * np.sin(45))
+    dataset['R60_X'] = (x * np.cos(60)) + (y * np.sin(60))
+    dataset['R60_Y'] = (y * np.cos(60)) - (x * np.sin(60))
+    return dataset
+
+def rotate_pca(dataset):
+    xy = dataset[['X','Y']]
+    coordinates = xy.values
+    pca_trans = PCA().fit(coordinates).transform(xy)
+    dataset['PCA_X'] = pca_trans[:,0]
+    dataset['PCA_Y'] = pca_trans[:,1]
+    return dataset
+
+def polar_coord(dataset):
+    x, y = dataset['X'], dataset['Y']
+    dataset['Polar_Rho'] = np.sqrt(x**2 + y**2)
+    dataset['Polar_Phi'] = np.arctan(y / x)
+    return dataset
+
+def encode_nearest_hav(dataset):
     x, y = dataset['X'], dataset['Y']
     # Add distance to the closest police station
-    dataset['NearestStation'] = np.array([
+    dataset['NearestStation_Hav'] = np.array([
         haversine(x, y, -122.409960, 37.798736),  # Central Station 
         haversine(x, y, -122.446261, 37.724694),  # Ingleside Station
         haversine(x, y, -122.432516, 37.780226),  # Northern Station
@@ -100,6 +137,63 @@ def encode_geospatial(dataset):
         haversine(x, y, -122.464462, 37.780016),  # Richmond Station 
         haversine(x, y, -122.481516, 37.743755)   # Taraval Station
         ]).min(axis = 0) # Take min index wise
+    return dataset
+
+def encode_nearest_man(dataset):
+    x, y = dataset['X'], dataset['Y']
+    # Add distance to the closest police station
+    dataset['NearestStation_Man'] = np.array([
+        manhattan(x, y, -122.409960, 37.798736),  # Central Station 
+        manhattan(x, y, -122.446261, 37.724694),  # Ingleside Station
+        manhattan(x, y, -122.432516, 37.780226),  # Northern Station
+        manhattan(x, y, -122.389411, 37.772382),  # Southern Station
+        manhattan(x, y, -122.412924, 37.783783),  # Tenderloin Station
+        manhattan(x, y, -122.397771, 37.729825),  # Bayview Station
+        manhattan(x, y, -122.421951, 37.763013),  # Mission Station
+        manhattan(x, y, -122.455391, 37.767835),  # Park Station
+        manhattan(x, y, -122.464462, 37.780016),  # Richmond Station 
+        manhattan(x, y, -122.481516, 37.743755)   # Taraval Station
+        ]).min(axis = 0) # Take min index wise
+    return dataset
+
+def encode_nearest_bearing_hav(dataset):
+    # TODO: Put this together with distance function to avoid repetition
+    x, y = dataset['X'], dataset['Y']
+    x_station = np.array([-122.409960, -122.446261, -122.432516, -122.389411, -122.412924, -122.397771, -122.421951, -122.455391, -122.464462, -122.481516])
+    y_station = np.array([37.798736, 37.724694, 37.780226, 37.772382, 37.783783, 37.729825, 37.763013, 37.767835, 37.780016, 37.743755])
+    min_index = np.array([
+        haversine(x, y, -122.409960, 37.798736),  # Central Station 
+        haversine(x, y, -122.446261, 37.724694),  # Ingleside Station
+        haversine(x, y, -122.432516, 37.780226),  # Northern Station
+        haversine(x, y, -122.389411, 37.772382),  # Southern Station
+        haversine(x, y, -122.412924, 37.783783),  # Tenderloin Station
+        haversine(x, y, -122.397771, 37.729825),  # Bayview Station
+        haversine(x, y, -122.421951, 37.763013),  # Mission Station
+        haversine(x, y, -122.455391, 37.767835),  # Park Station
+        haversine(x, y, -122.464462, 37.780016),  # Richmond Station 
+        haversine(x, y, -122.481516, 37.743755)   # Taraval Station
+        ]).argmin(axis = 0) # Take argmin index wise
+    dataset['NearestStationBearing_Hav'] = bearing(x, y, x_station[min_index], y_station[min_index])
+    return dataset
+
+def encode_nearest_bearing_man(dataset):
+    # TODO: Put this together with distance function to avoid repetition
+    x, y = dataset['X'], dataset['Y']
+    x_station = np.array([-122.409960, -122.446261, -122.432516, -122.389411, -122.412924, -122.397771, -122.421951, -122.455391, -122.464462, -122.481516])
+    y_station = np.array([37.798736, 37.724694, 37.780226, 37.772382, 37.783783, 37.729825, 37.763013, 37.767835, 37.780016, 37.743755])
+    min_index = np.array([
+        manhattan(x, y, -122.409960, 37.798736),  # Central Station 
+        manhattan(x, y, -122.446261, 37.724694),  # Ingleside Station
+        manhattan(x, y, -122.432516, 37.780226),  # Northern Station
+        manhattan(x, y, -122.389411, 37.772382),  # Southern Station
+        manhattan(x, y, -122.412924, 37.783783),  # Tenderloin Station
+        manhattan(x, y, -122.397771, 37.729825),  # Bayview Station
+        manhattan(x, y, -122.421951, 37.763013),  # Mission Station
+        manhattan(x, y, -122.455391, 37.767835),  # Park Station
+        manhattan(x, y, -122.464462, 37.780016),  # Richmond Station 
+        manhattan(x, y, -122.481516, 37.743755)   # Taraval Station
+        ]).argmin(axis = 0) # Take argmin index wise
+    dataset['NearestStationBearing_Man'] = bearing(x, y, x_station[min_index], y_station[min_index])
     return dataset
 
 def center_scale_features(dataset):
@@ -124,6 +218,19 @@ def haversine(x1, y1, x2, y2):
     lon1, lat1, lon2, lat2 = map(np.radians, [x1, y1, x2, y2])
     h = np.sin((lat2 - lat1) / 2)**2 + np.cos(lat1) * np.cos(lat2) * np.sin((lon2 - lon1) / 2)**2
     return 2 * 6378.137 * np.arcsin(np.sqrt(h))
+
+# Manhattan distance L1-norm
+def manhattan(x1, y1, x2, y2):
+  lat_dist = haversine(x1, y1, x1, y2)
+  lon_dist = haversine(x1, y1, x2, y1)
+  return lat_dist + lon_dist
+
+# Bearing degree
+def bearing(x1, y1, x2, y2):
+  x1, y1, x2, y2 = map(np.radians, [x1, y1, x2, y2]) 
+  y = np.sin(x2 - x1) * np.cos(y2)
+  x = np.cos(y1) * np.sin(y2) - np.sin(y1) * np.cos(y2) * np.cos(x2 - x1)
+  return np.degrees(np.arctan2(y, x))
 
 # Progress tracker helpers
 
@@ -150,12 +257,12 @@ def end():
     print(f'\nElapsed time: {round(time.time() - tic, 3)} second(s)', flush = True)
 
 
-# if __name__== "__main__":
-#     try:
-#         os.chdir(os.path.dirname(os.path.abspath(__file__)))
-#         zipFile = ZipFile('data/sf-crime.zip')
-#         dataset = pd.read_csv(zipFile.open('train.csv'))
-#         main_clean(dataset, center_scale = True)
-#     except Exception as e:
-#         print(e)
-#         print("Can't find csv!")
+if __name__== "__main__":
+    try:
+        os.chdir(os.path.dirname(os.path.abspath(__file__)))
+        zipFile = ZipFile('data/sf-crime.zip')
+        dataset = pd.read_csv(zipFile.open('train.csv'))
+        main_clean(dataset, center_scale = True)
+    except Exception as e:
+        print(e)
+        print("Can't find csv!")
