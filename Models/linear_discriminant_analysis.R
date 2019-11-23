a243428a539ac5ce6af753cb70a28be0c3c469ed
irkaal <irkaalv@gmail.com>
2019-11-22 16:31:06 -0800

    Update lda and sgd classifier

--- Models/linear_discriminant_analysis.R
+++ Models/linear_discriminant_analysis.R
@@ -6,21 +6,21 @@ library(caret)
 library(MASS)
 
 # Load the cleaned data
-path <- unzip('../data/clean_centered_final.zip', 'clean_centered_final.csv')
+path <- unzip('../data/clean_final.zip', 'clean_final.csv')
 train <- fread(path); invisible(file.remove(path))
 y_train <- factor(make.names(train$Category))
 X_train <- train[, -which(names(train) == 'Category'), with = F]
 rm(train)
 
 # Log Loss
-ctrl <- trainControl(method = 'repeatedcv', number = 2, repeats = 5,
+ctrl <- trainControl(method = 'repeatedcv', number = 3, repeats = 5,
                      classProbs = T, summaryFunction = mnLogLoss,
                      verboseIter = T, allowParallel = F)
 set.seed(2019)
 lda_model <- train(x = X_train, y = y_train, method = 'lda', metric = 'logLoss', trControl = ctrl)
 
 # Accuracy and Kappa
-ctrl <- trainControl(method = 'repeatedcv', number = 2, repeats = 5,
+ctrl <- trainControl(method = 'repeatedcv', number = 3, repeats = 5,
                      verboseIter = T, allowParallel = F)
 set.seed(2019)
 lda_model_acc <- train(x = X_train, y = y_train, method = 'lda', metric = 'Accuracy', trControl = ctrl)
