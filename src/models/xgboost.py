86325404f03f5aafe6645228b58f5af0386c2a07
irkaal <irkaalv@gmail.com>
2019-11-24 17:48:39 -0800

    Update xgboost model

--- src/models/xgboost.py
+++ src/models/xgboost.py
@@ -1,71 +1,66 @@
+import csv
 import numpy as np
 import pandas as pd
 import xgboost as xgb
-from sklearn.model_selection import GridSearchCV, RepeatedStratifiedKFold
 from hyperopt import hp, fmin, tpe, STATUS_OK
+from zipfile import ZipFile
 
-# Load train data
-train_data = pd.read_csv('data/clean_final.csv')
-y_train = train_data['Category'].astype('category')
-X_train = train_data.drop('Category', axis = 1)
-dtrain = xgb.DMatrix(data = X_train, label = y_train)
+# Load Data
+train_data = pd.read_csv(ZipFile('data/processed/train.zip').open('train.csv'))
+dtrain = xgb.DMatrix(data = train_data.drop('Category', axis = 1), 
+                     label = train_data['Category'].values)
 
-
-# Hyperparameter search with Bayesian Optimization
-
-# out_file = 'gbm_trials.csv'
-# of_connection = open(out_file, 'w')
-# writer = csv.writer(of_connection)
-# writer.writerow(['loss', 'params', 'iteration', 'estimators'])
-# of_connection.close()
+# Bayesian Optimization
+out_file = 'xgb_trials.csv'
+of_connection = open(out_file, 'w')
+writer = csv.writer(of_connection)
+writer.writerow(['loss', 'params', 'iteration', 'estimators'])
+of_connection.close()
+ITERATION = 0
 
 def score(params):
-    num_boost = int(params['n_estimators'])
+    nbr = int(params['n_estimators'])
     del params['n_estimators']
-    best = xgb.cv(params, dtrain, num_boost_round = num_boost, 
-                  nfold = 2, stratified = True, metrics = 'mlogloss')
+    best = xgb.cv(params, dtrain, num_boost_round = nbr, nfold = 2, stratified = True, metrics = 'mlogloss', seed = 2019)
     loss = best['test-mlogloss-mean'].tail(1)
-    
     # Save result
-    # of_connection = open(out_file, 'a')
-    # writer = csv.writer(of_connection)
-    # writer.writerow([loss, params, ITERATION, n_estimators])
-    # of_connection.close()
-    
+    global ITERATION
+    ITERATION += 1
+    of_connection = open(out_file, 'a')
+    writer = csv.writer(of_connection)
+    writer.writerow([loss, params, ITERATION, nbr])
+    of_connection.close()
     return {'loss': loss, 'status': STATUS_OK}
- 
+
 def optimize(max_evals):
     space = {
-        'n_estimators': hp.quniform('n_estimators', 200, 600, 1),
-        'eta': hp.quniform('eta', 0.025, 0.25, 0.025),
-        'max_depth':  hp.choice('max_depth', np.arange(1, 14, dtype=int)),
+        'n_estimators': hp.quniform('n_estimators', 500, 1000, 1),
+        'tree_method': 'hist',
+        'eta': hp.quniform('eta', 0.05, 0.2, 0.025),
+        'max_depth':  hp.choice('max_depth', np.arange(3, 13, dtype=int)),
         'min_child_weight': hp.quniform('min_child_weight', 1, 10, 1),
-        'subsample': hp.quniform('subsample', 0.7, 1, 0.05),
-        'gamma': hp.quniform('gamma', 0.5, 1, 0.05),
-        'colsample_bytree': hp.quniform('colsample_bytree', 0.7, 1, 0.05),
-        'alpha' :  hp.quniform('alpha', 0, 10, 1),
+        'subsample': hp.quniform('subsample', 0.8, 1, 0.05),
+        'gamma': hp.choice('gamma', np.array([0, 1, 5])),
+        'colsample_bytree': hp.quniform('colsample_bytree', 0.3, 1, 0.05),
+        'alpha' : hp.quniform('alpha', 0, 10, 1),
         'lambda': hp.quniform('lambda', 1, 2, 0.1),
         'objective': 'multi:softmax', 
         'num_class': 39, 
         'seed': 2019
     }
-    best = fmin(score, space, algo = tpe.suggest, max_evals = max_evals)
-    return best
-best_param = optimize(max_evals = 10)
-print(best_param)
+    best_param = fmin(score, space, algo = tpe.suggest, max_evals = max_evals)
+    return best_param
 
+best_param = optimize(max_evals = 50)
 
-# Hyperparameter grid search
-skf = RepeatedStratifiedKFold(n_splits = 3, n_repeats = 5, random_state = 2019)
-parameters = {
-    'n_estimators': [100],
-    'objective': ['multi:softmax'],
-    'verbosity': [1],
-    'n_jobs': [-1],
-    'random_state': [2019]
-}
-grid_search = GridSearchCV(estimator = xgb.XGBClassifier(), param_grid = parameters, scoring = 'neg_log_loss',
-                            cv = skf, n_jobs = -1, refit = False, verbose = 1)                            
-grid_search.fit(X_train, y_train)
-pd.set_option('display.max_columns', 30)
-print(pd.DataFrame(grid_search.cv_results_))
+# params = {
+#     'alpha': 5.0,
+#     'colsample_bytree': 0.8,
+#     'eta': 0.05,
+#     'gamma': 0.75,
+#     'lambda': 1.8,
+#     'max_depth': 12,
+#     'min_child_weight': 3.0,
+#     'n_estimators': 536.0,
+#     'subsample': 0.75
+# }
