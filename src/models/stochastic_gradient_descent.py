f74e90af752e0d5946c3b5c6e944938e746688a9
irkaal <irkaalv@gmail.com>
2019-11-30 21:04:55 -0800

    Fix test data scaling mistake

--- src/models/stochastic_gradient_descent.py
+++ src/models/stochastic_gradient_descent.py
@@ -2,15 +2,19 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import GridSearchCV, RepeatedStratifiedKFold
+from sklearn.preprocessing import StandardScaler
 from zipfile import ZipFile
 
 
 # Load Data
-train_data = pd.read_csv(ZipFile('data/processed/train_centered.zip').open('train.csv'))
+train_data = pd.read_csv(ZipFile('data/processed/train.zip').open('train.csv'))
 X_train = train_data.drop(['Category', 
                            '2010-2012', '06:00-17:59', 
                            'PdDistrict_TARAVAL', 'Patrol_Division',
                            'Polar_Rho', 'Polar_Phi', 'X_R30', 'Y_R30', 'X_R60', 'Y_R60', 'XY_PCA1', 'XY_PCA2'], axis = 1)
+X_train_columns = X_train.columns
+scaler = StandardScaler().fit(X_train)
+X_train = pd.DataFrame(scaler.transform(X_train), columns = X_train_columns)
 category = pd.factorize(train_data['Category'], sort = True)
 y_train = category[0]
 
@@ -63,9 +67,11 @@ X_test = X_test.drop(['Id',
                      '2010-2012', '06:00-17:59', 
                      'PdDistrict_TARAVAL', 'Patrol_Division',
                      'Polar_Rho', 'Polar_Phi', 'X_R30', 'Y_R30', 'X_R60', 'Y_R60', 'XY_PCA1', 'XY_PCA2'], axis = 1)
+X_test_columns = X_test.columns
+X_test = pd.DataFrame(scaler.transform(X_test), columns = X_test_columns)
 
 # Logistic loss
-clf = SGDClassifier(loss='log', alpha=1e-1, max_iter=5000, random_state=2019, verbose=2)
+clf = SGDClassifier(loss='log', alpha=1e-1, max_iter=5000, random_state=2019, verbose=2, n_jobs=-1)
 clf.fit(X_train, y_train)
 pred_proba = clf.predict_proba(X_test)
 result = pd.DataFrame(pred_proba)
@@ -75,7 +81,7 @@ result.columns = column_names
 result.to_csv('submission.csv', index = False)
 
 # Modified huber loss
-clf = SGDClassifier(loss='modified_huber', alpha=1e-2, fit_intercept=False, max_iter=5000, random_state=2019, verbose=2)
+clf = SGDClassifier(loss='modified_huber', alpha=1e-2, fit_intercept=False, max_iter=5000, random_state=2019, verbose=2, n_jobs=-1)
 clf.fit(X_train, y_train)
 pred_proba = clf.predict_proba(X_test)
 result = pd.DataFrame(pred_proba)
