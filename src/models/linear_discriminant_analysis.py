Incorporate pipeline to ensure correct feature scaling procedure during CV

src/models/linear_discriminant_analysis.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
+from sklearn.pipeline import make_pipeline 
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.model_selection import GridSearchCV, RepeatedStratifiedKFold
-from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import RepeatedStratifiedKFold, cross_val_score
 from sklearn.preprocessing import StandardScaler
 from zipfile import ZipFile
 
@@ -14,29 +14,20 @@ category = pd.factorize(train_data['Category'], sort = True)
 y_train = category[0]
 
 # Cross validation
-rskf = RepeatedStratifiedKFold(n_splits = 5, n_repeats = 5, random_state = 2019)
-lda_clf = make_pipeline(StandardScaler(), 
-                        GridSearchCV(estimator = LinearDiscriminantAnalysis(), 
-                                     param_grid = {'solver': ['svd']}, 
-                                     scoring = 'neg_log_loss',
-                                     cv = rskf, 
-                                     n_jobs = -1, 
-                                     verbose = 2))
-lda_clf.fit(X_train, y_train)
-result = pd.DataFrame(lda_clf._final_estimator.cv_results_)
-print(result[['mean_test_score', 'rank_test_score']])
+rskf = RepeatedStratifiedKFold(n_splits = 2, n_repeats = 5, random_state = 2019)
+lda_clf = make_pipeline(StandardScaler(), LinearDiscriminantAnalysis())
+result = cross_val_score(lda_clf, X_train, y_train, scoring = 'neg_log_loss', cv = rskf, n_jobs = -1, verbose = 2)
+print(-np.mean(result))
 
 # Prediction
-X_train_columns = X_train.columns
 scaler = StandardScaler().fit(X_train)
-X_train = pd.DataFrame(scaler.transform(X_train), columns = X_train_columns)
+X_train = pd.DataFrame(scaler.transform(X_train), columns = X_train.columns)
 
 X_test = pd.read_csv(ZipFile('data/processed/test.zip').open('test.csv'))
 crime_id = X_test['Id']
 X_test = X_test.drop(['Id', '2010-2012', '0600-1759', 'PdDistrict_TARAVAL', 'Patrol_Division',
                       'Polar_Rho', 'Polar_Phi', 'X_R30', 'Y_R30', 'X_R60', 'Y_R60', 'XY_PCA1', 'XY_PCA2'], axis = 1)
-X_test_columns = X_test.columns
-X_test = pd.DataFrame(scaler.transform(X_test), columns = X_test_columns)
+X_test = pd.DataFrame(scaler.transform(X_test), columns = X_test.columns)
 
 clf = LinearDiscriminantAnalysis().fit(X_train, y_train)
 pred_proba = clf.predict_proba(X_test)

src/models/quadratic_discriminant_analysis.py
@@ -1,57 +1,43 @@
 import numpy as np
 import pandas as pd
+from sklearn.pipeline import make_pipeline 
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
-from sklearn.model_selection import GridSearchCV, RepeatedStratifiedKFold
+from sklearn.model_selection import RepeatedStratifiedKFold, cross_val_score
 from sklearn.preprocessing import StandardScaler
 from zipfile import ZipFile
 
-
 # Load Data
 train_data = pd.read_csv(ZipFile('data/processed/train.zip').open('train.csv'))
-X_train = train_data.drop(['Category', 
-                           '2003-2005', '2013-2016', '00:00-05:59', '18:00-23:59', '06:00-17:59',
-                           'Polar_Rho', 'Polar_Phi', 'XY_PCA1', 'XY_PCA2',
-                           'Patrol_Division', 
+X_train = train_data.drop(['Category', '2003-2005', '2013-2016', '0000-0559', '1800-2359', '0600-1759',
+                           'Polar_Rho', 'Polar_Phi', 'XY_PCA1', 'XY_PCA2', 'Patrol_Division', 
                            'PdDistrict_BAYVIEW', 'PdDistrict_CENTRAL', 'PdDistrict_INGLESIDE', 'PdDistrict_MISSION', 'PdDistrict_NORTHERN', 
                            'PdDistrict_PARK', 'PdDistrict_RICHMOND', 'PdDistrict_SOUTHERN', 'PdDistrict_TARAVAL', 'PdDistrict_TENDERLOIN'], axis = 1)
-X_train_columns = X_train.columns
-scaler = StandardScaler().fit(X_train)
-X_train = pd.DataFrame(scaler.transform(X_train), columns = X_train_columns)
 category = pd.factorize(train_data['Category'], sort = True)
 y_train = category[0]
 
-
 # Cross validation
-pd.set_option('display.max_columns', 30)
-skf = RepeatedStratifiedKFold(n_splits = 2, n_repeats = 3, random_state = 2019)
-parameters = {
-    'tol': [1e-4]
-}
-grid_search = GridSearchCV(estimator = QuadraticDiscriminantAnalysis(), param_grid = parameters, scoring = 'neg_log_loss',
-                            cv = skf, n_jobs = -1, refit = False, verbose = 2)
-grid_search.fit(X_train, y_train)
-print(pd.DataFrame(grid_search.cv_results_))
+rskf = RepeatedStratifiedKFold(n_splits = 2, n_repeats = 5, random_state = 2019)
+qda_clf = make_pipeline(StandardScaler(), QuadraticDiscriminantAnalysis())
+result = cross_val_score(qda_clf, X_train, y_train, scoring = 'neg_log_loss', cv = rskf, n_jobs = -1, verbose = 2)
+print(-np.mean(result))
 
+# Prediction
+scaler = StandardScaler().fit(X_train)
+X_train = pd.DataFrame(scaler.transform(X_train), columns = X_train.columns)
 
-# Predict using final model
 X_test = pd.read_csv(ZipFile('data/processed/test.zip').open('test.csv'))
 crime_id = X_test['Id']
-X_test = X_test.drop(['Id', 
-                      '2003-2005', '2013-2016', '0000-0559', '1800-2359', '0600-1759',
-                      'Polar_Rho', 'Polar_Phi', 'XY_PCA1', 'XY_PCA2',
-                      'Patrol_Division', 
+X_test = X_test.drop(['Id', '2003-2005', '2013-2016', '0000-0559', '1800-2359', '0600-1759',
+                      'Polar_Rho', 'Polar_Phi', 'XY_PCA1', 'XY_PCA2', 'Patrol_Division', 
                       'PdDistrict_BAYVIEW', 'PdDistrict_CENTRAL', 'PdDistrict_INGLESIDE', 'PdDistrict_MISSION', 'PdDistrict_NORTHERN', 
                       'PdDistrict_PARK', 'PdDistrict_RICHMOND', 'PdDistrict_SOUTHERN', 'PdDistrict_TARAVAL', 'PdDistrict_TENDERLOIN'], axis = 1)
-X_test_columns = X_test.columns
-X_test = pd.DataFrame(scaler.transform(X_test), columns = X_test_columns)
+X_test = pd.DataFrame(scaler.transform(X_test), columns = X_test.columns)
+
 clf = QuadraticDiscriminantAnalysis()
 clf.fit(X_train, y_train)
 pred_proba = clf.predict_proba(X_test)
-
-
-# Save results
 result = pd.DataFrame(pred_proba)
 result.insert(0, 'Id', crime_id)
 column_names = np.insert(category[1], 0, 'Id')
 result.columns = column_names
-result.to_csv('submission.csv', index = False)
+result.to_csv('qda_submission.csv', index = False)

src/models/stochastic_gradient_descent.py
@@ -1,89 +1,149 @@
 import numpy as np
 import pandas as pd
+from sklearn.calibration import CalibratedClassifierCV
+from sklearn.kernel_approximation import RBFSampler
 from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import GridSearchCV, RepeatedStratifiedKFold
+from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from zipfile import ZipFile
 
-
 # Load Data
 train_data = pd.read_csv(ZipFile('data/processed/train.zip').open('train.csv'))
-X_train = train_data.drop(['Category', 
-                           '2010-2012', '0600-1759', 
-                           'PdDistrict_TARAVAL', 'Patrol_Division',
+X_train = train_data.drop(['Category', '2010-2012', '0600-1759', 'PdDistrict_TARAVAL', 'Patrol_Division',
                            'Polar_Rho', 'Polar_Phi', 'X_R30', 'Y_R30', 'X_R60', 'Y_R60', 'XY_PCA1', 'XY_PCA2'], axis = 1)
-X_train_columns = X_train.columns
-scaler = StandardScaler().fit(X_train)
-X_train = pd.DataFrame(scaler.transform(X_train), columns = X_train_columns)
 category = pd.factorize(train_data['Category'], sort = True)
 y_train = category[0]
 
-pd.set_option('display.max_columns', 30)
-skf = RepeatedStratifiedKFold(n_splits = 2, n_repeats = 3, random_state = 2019)
-
+# Cross Validation
+rskf = RepeatedStratifiedKFold(n_splits = 2, n_repeats = 1, random_state = 2019)
+pipe = make_pipeline(StandardScaler(), 
+                     SGDClassifier())
+calibrated_pipe = make_pipeline(StandardScaler(), 
+                                CalibratedClassifierCV(base_estimator = SGDClassifier(), 
+                                                       method = 'sigmoid', 
+                                                       cv = 2))
 
 # Logistic Loss
 parameters = {
-    'loss': ['log'],
-    'penalty': ['l2', 'l1'],
-    'fit_intercept': [True, False], 
-    'alpha': [1e-4, 1e-3, 1e-2, 1e-1],  
-    'max_iter': [10000], 
-    'random_state': [2019]
+    'sgdclassifier__loss': ['log'],
+    'sgdclassifier__penalty': ['l2', 'l1'],
+    'sgdclassifier__fit_intercept': [True, False], 
+    'sgdclassifier__alpha': [1e-4, 1e-3, 1e-2, 1e-1],  
+    'sgdclassifier__max_iter': [10000], 
+    'sgdclassifier__random_state': [2019]
 }
-
-# Log loss measure
-grid_search = GridSearchCV(estimator = SGDClassifier(), param_grid = parameters, scoring = 'neg_log_loss',
-                           cv = skf, n_jobs = -1, refit = False, verbose = 2)
+grid_search = GridSearchCV(estimator = pipe, 
+                           param_grid = parameters, 
+                           scoring = 'neg_log_loss',
+                           cv = rskf, 
+                           refit = False,
+                           n_jobs = -1, 
+                           verbose = 2)
 grid_search.fit(X_train, y_train)
-print(pd.DataFrame(grid_search.cv_results_))
-
-
+result = pd.DataFrame(grid_search.cv_results_)
+print(result[['params', 'mean_test_score', 'rank_test_score']])
 
 # Modified Huber Loss
 parameters = {
-    'loss': ['modified_huber'],
-    'penalty': ['l2'], 
-    'alpha': [0.01], 
-    'fit_intercept': [False], 
-    'max_iter': [5000],
-    'random_state': [2019]
+    'sgdclassifier__loss': ['modified_huber'],
+    'sgdclassifier__penalty': ['l2'], 
+    'sgdclassifier__alpha': [0.01], 
+    'sgdclassifier__fit_intercept': [False], 
+    'sgdclassifier__max_iter': [5000],
+    'sgdclassifier__random_state': [2019]
+}
+pipe = make_pipeline(StandardScaler(), 
+                     SGDClassifier())
+grid_search = GridSearchCV(estimator = pipe, 
+                           param_grid = parameters, 
+                           scoring = 'neg_log_loss',
+                           cv = rskf, 
+                           refit = False,
+                           n_jobs = -1, 
+                           verbose = 2)
+grid_search.fit(X_train, y_train)
+result = pd.DataFrame(grid_search.cv_results_)
+print(result[['params', 'mean_test_score', 'rank_test_score']])
+
+# Hinge Loss
+parameters = {
+    'calibratedclassifiercv__base_estimator__loss': ['hinge'],
+    'calibratedclassifiercv__base_estimator__alpha': [1e0], 
+    'calibratedclassifiercv__base_estimator__penalty': ['l2'],
+    'calibratedclassifiercv__base_estimator__tol': [1e-3],
+    'calibratedclassifiercv__base_estimator__fit_intercept': [True], 
+    'calibratedclassifiercv__base_estimator__random_state': [2019],
+    'calibratedclassifiercv__base_estimator__max_iter': [5000]
 }
 
-# Log loss measure
-grid_search = GridSearchCV(estimator = SGDClassifier(), param_grid = parameters, scoring = 'neg_log_loss',
-                           cv = skf, n_jobs = -1, refit = False, verbose = 2)
+grid_search = GridSearchCV(estimator = calibrated_pipe, 
+                           param_grid = parameters, 
+                           scoring = 'neg_log_loss',
+                           cv = rskf, 
+                           refit = False,
+                           n_jobs = -1,
+                           verbose = 2)
 grid_search.fit(X_train, y_train)
-print(pd.DataFrame(grid_search.cv_results_))
+result = pd.DataFrame(grid_search.cv_results_)
+print(result[['params', 'mean_test_score', 'rank_test_score']])
 
+# Prediction 
+scaler = StandardScaler().fit(X_train)
+X_train = pd.DataFrame(scaler.transform(X_train), columns = X_train.columns)
 
-
-# Prediction
 X_test = pd.read_csv(ZipFile('data/processed/test.zip').open('test.csv'))
 crime_id = X_test['Id']
-X_test = X_test.drop(['Id', 
-                     '2010-2012', '0600-1759', 
-                     'PdDistrict_TARAVAL', 'Patrol_Division',
-                     'Polar_Rho', 'Polar_Phi', 'X_R30', 'Y_R30', 'X_R60', 'Y_R60', 'XY_PCA1', 'XY_PCA2'], axis = 1)
-X_test_columns = X_test.columns
-X_test = pd.DataFrame(scaler.transform(X_test), columns = X_test_columns)
+X_test = X_test.drop(['Id', '2010-2012', '0600-1759', 'PdDistrict_TARAVAL', 'Patrol_Division',
+                      'Polar_Rho', 'Polar_Phi', 'X_R30', 'Y_R30', 'X_R60', 'Y_R60', 'XY_PCA1', 'XY_PCA2'], axis = 1)
+X_test = pd.DataFrame(scaler.transform(X_test), columns = X_test.columns)
 
-# Logistic loss
-clf = SGDClassifier(loss='log', alpha=1e-1, max_iter=5000, random_state=2019, verbose=2, n_jobs=-1)
+# Logistic Loss
+logistic_clf = SGDClassifier(loss = 'log', 
+                             alpha = 1e-1, 
+                             max_iter = 5000, 
+                             random_state = 2019, 
+                             verbose = 2, 
+                             n_jobs = -1)
+logistic_clf.fit(X_train, y_train)
+pred_proba = logistic_clf.predict_proba(X_test)
+result = pd.DataFrame(pred_proba)
+result.insert(0, 'Id', crime_id)
+column_names = np.insert(category[1], 0, 'Id')
+result.columns = column_names
+result.to_csv('logistic_sgd_submission.csv', index = False)
+
+# Modified Huber Loss
+huber_clf = SGDClassifier(loss = 'modified_huber', 
+                          alpha = 1e-2, 
+                          fit_intercept = False, 
+                          max_iter = 5000, 
+                          random_state = 2019, 
+                          verbose = 2, 
+                          n_jobs = -1)
+huber_clf.fit(X_train, y_train)
+pred_proba = huber_clf.predict_proba(X_test)
+result = pd.DataFrame(pred_proba)
+result.insert(0, 'Id', crime_id)
+column_names = np.insert(category[1], 0, 'Id')
+result.columns = column_names
+result.to_csv('huber_sgd_submission.csv', index = False)
+
+# Hinge Loss
+clf = CalibratedClassifierCV(SGDClassifier(loss = 'hinge', 
+                                           penalty = 'l2', 
+                                           tol = 1e-3, 
+                                           alpha = 1e0, 
+                                           max_iter = 5000, 
+                                           random_state = 2019,
+                                           verbose = 2, 
+                                           n_jobs = -1), 
+                             method = 'sigmoid', 
+                             cv = 2)
 clf.fit(X_train, y_train)
 pred_proba = clf.predict_proba(X_test)
 result = pd.DataFrame(pred_proba)
 result.insert(0, 'Id', crime_id)
 column_names = np.insert(category[1], 0, 'Id')
 result.columns = column_names
-result.to_csv('submission.csv', index = False)
-
-# Modified huber loss
-clf = SGDClassifier(loss='modified_huber', alpha=1e-2, fit_intercept=False, max_iter=5000, random_state=2019, verbose=2, n_jobs=-1)
-clf.fit(X_train, y_train)
-pred_proba = clf.predict_proba(X_test)
-result = pd.DataFrame(pred_proba)
-result.insert(0, 'Id', crime_id)
-column_names = np.insert(category[1], 0, 'Id')
-result.columns = column_names
-result.to_csv('submission.csv', index = False)
+result.to_csv('hinge_sgd_submission.csv', index = False)